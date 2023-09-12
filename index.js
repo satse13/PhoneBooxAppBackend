0134a460ccb1237be6cfef5269d765689dad9b6a
satse13 <atobiasangeles@gmail.com>
2023-09-12 10:23:03 +0200

    Ejercicio 3.4 completado

--- index.js
+++ index.js
@@ -46,7 +46,12 @@ app.get('/info', (req,res) => {
     <p>${new Date()}</p>`)
 })
 
-
+app.delete('/api/persons/:id',(req,res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+    
+    res.status(204).end()
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
