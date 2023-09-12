018def0c3512758d95a148b08731e1a25ffde946
satse13 <atobiasangeles@gmail.com>
2023-09-12 10:02:01 +0200

    Ejercicio 3.2 completado

--- index.js
+++ index.js
@@ -32,6 +32,10 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/info', (req,res) => {
+    res.send(`<h1>Phonebook has info for ${persons.length} people</h1>
+    <p>${new Date()}</p>`)
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
