57c288a219240895e6a40ecf25776193877c85bc
satse13 <atobiasangeles@gmail.com>
2023-09-13 17:59:46 +0200

    3.9 completado

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 var morgan = require('morgan')
+
+app.use(cors())
+
 app.use(express.json());
+
 app.use(morgan((tokens, req, res) => {
     return [
       tokens.method(req, res),
@@ -102,7 +107,7 @@ const generateId = () => {
     return maxId + 1
 }
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
