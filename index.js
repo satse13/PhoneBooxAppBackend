97e58b8243738d8ef8a9b7629d7a568150ab3ddd
satse13 <atobiasangeles@gmail.com>
2023-09-20 18:06:39 +0200

    Exportada logica base de datos a su modulo

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
-require('dotenv').config()
 const express = require('express')
-const cors = require('cors')
 const app = express()
-const mongoose = require('mongoose')
+const cors = require('cors')
+require('dotenv').config()
 
-const url = process.env.MONGODB_URI
-mongoose.set('strictQuery',false)
-mongoose.connect(url)
-
-const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-})
-
-personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
-
-const Person = mongoose.model('Person', personSchema)
-
+const Person = require('./models/person')
 
 var morgan = require('morgan')
 
