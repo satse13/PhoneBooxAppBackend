9ffce765554bae80b5d26917ba5b7d389adf50f6
satse13 <atobiasangeles@gmail.com>
2023-09-20 18:46:43 +0200

    Añadido actualizacion de objetos en base de dates por frontend

--- index.js
+++ index.js
@@ -85,6 +85,23 @@ app.delete('/api/people/:id',(req,res,next) => {
  
 })
 
+app.put('/api/people/:id', (req,res,next) => {
+
+    const body = req.body
+
+    const person = {
+        name : body.name,
+        number : body.number
+    }
+
+    Person.findByIdAndUpdate(req.params.id, person, {new:true})
+        .then(updatedPerson => {
+            res.json(updatedPerson)
+        })
+        .catch(error => next(error))
+
+})
+
 app.use(unknownEndpoint)
 app.use(errorHandler)
 
