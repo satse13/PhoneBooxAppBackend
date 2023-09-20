bf7d6a00397c5d18bcb3a031d5f1077c70887d2c
satse13 <atobiasangeles@gmail.com>
2023-09-20 18:24:59 +0200

    Añadido manejo de errores por middleware

--- index.js
+++ index.js
@@ -5,49 +5,38 @@ require('dotenv').config()
 
 const Person = require('./models/person')
 
-var morgan = require('morgan')
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.use(cors())
-app.use(express.static('dist'))
-
 app.use(express.json());
-
-app.use(morgan((tokens, req, res) => {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms',
-      JSON.stringify(req.body)
-    ].join(' ')
-}))
-
-
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
-})
+app.use(requestLogger)
+app.use(express.static('dist'))
 
 app.get('/api/people', (req, res) => {
     Person.find({}).then(people => {
         res.json(people)
     })
-})
-
-app.get('/api/people/:id', (req,res,next) => {
-    Person.findById(req.params.id)
-    .then(person => {
-        person
-         ? res.json(person)
-         : res.status(404).end()
-    })
-    .catch(error => next(error))
-    
-})
-
-app.get('/info', (req,res) => {
-    res.send(`<h1>Phonebook has info for ${4} people</h1>
-    <p>${new Date()}</p>`)
 })
 
 app.post('/api/people',(req,res) => {
@@ -69,12 +58,22 @@ app.post('/api/people',(req,res) => {
         name: body.name,
         number: body.number,
     })
-    console.log({newPerson}) // comentario prueba
 
     newPerson.save().then(savedPerson => {
         res.json(savedPerson)
     })
 })
+
+app.get('/api/people/:id', (req,res,next) => {
+    Person.findById(req.params.id)
+    .then(person => {
+        person
+         ? res.json(person)
+         : res.status(404).end()
+    })
+    .catch(error => next(error))
+})
+
 
 app.delete('/api/people/:id',(req,res,next) => {
 
@@ -86,9 +85,8 @@ app.delete('/api/people/:id',(req,res,next) => {
  
 })
 
-app.use((request,response)=> {
-    response.status(404).end()
-})
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 
